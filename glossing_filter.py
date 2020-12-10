3ff35633feb245725e14c7995642343b2de305e6
Retr0327 <48786838+Retr0327@users.noreply.github.com>
2020-12-10 19:02:32 +0800

    Update glossing_filter.py

--- glossing_filter.py
+++ glossing_filter.py
@@ -23,37 +23,39 @@ else:
     for num_repeat in num_count: 
         if num_count.count(num_repeat)>1:
             count[num_repeat]=num_count.count(num_repeat)
-    print(count)
-            
-    for d in contain:
-        if contents[d+1].startswith("."):
-            contents[d+2]=". "+contents[d+2]
-            contents[d+3]=". "+contents[d+3]
+    if bool(count) is True:
+        print(count)
+    if bool(count) is False:
+        print("沒有重複數值")
+        for d in contain:
+            if contents[d+1].startswith("."):
+                contents[d+2]=". "+contents[d+2]
+                contents[d+3]=". "+contents[d+3]
 
-    contain_1=[y+1 for y in contain]
-    contain_2=[]
-    for z in contain_1:
-        original_line=contents[z]
-        glb_=contents[z+1]
-        glc_=contents[z+2]
-        clean_data=re.sub(r'[<>]', "", original_line).replace("^","")
-        clean_data=re.sub(r'(L\d.)', "", clean_data)
-        clean_data=re.sub(r"\b\=\b", "", clean_data) 
-        clean_data=re.sub(r"[\[\]]", "", clean_data) 
-        clean_data=re.sub(r"\-\b", "", clean_data) 
-        clean_data=re.sub(r"(?<=a)\.\b"," ", clean_data)      #delete the dot between words (e.g. daha.ka)
-        clean_data=re.sub(r'[\s]+', ' ', clean_data)
-        original_line=re.sub(r'[\s]+', ' ', original_line)
-        contents[z]=clean_data.replace("L@","")+"\n"+original_line+"\n"
-        glpre=clean_data
-        gla=original_line
-        glb=re.sub(r"[\s]+", " ", glb_)+"\n"   # glossing b
-        glc=re.sub(r"[\s]+", " ", glc_)+"\n"    # glossing c
-        contents[z+1]=glb
-        contents[z+2]=glc
-    a_file = open(file_name+".txt", "w",)
-    a_file = open(file_name+".txt", "w", encoding="utf-8")
-    a_file.writelines(contents)
-    a_file.close()
-    
-    
+        contain_1=[y+1 for y in contain]
+        contain_2=[]
+        for z in contain_1:
+            original_line=contents[z]
+            glb_=contents[z+1]
+            glc_=contents[z+2]
+            clean_data=re.sub(r'[<>]', "", original_line).replace("^","")
+            clean_data=re.sub(r'(L\d.)', "", clean_data)
+            clean_data=re.sub(r"\b\=\b", "", clean_data)    #delete clitics
+            clean_data=re.sub(r"[\[\]]", "", clean_data) 
+            clean_data=re.sub(r"\-\b", "", clean_data) 
+            clean_data=re.sub(r"(?<=a)\.\b"," ", clean_data)      #delete the dot between words (e.g. daha.ka)
+            clean_data=re.sub(r'[\s]+', ' ', clean_data)
+            original_line=re.sub(r'[\s]+', ' ', original_line)
+            contents[z]=clean_data.replace("L@","")+"\n"+original_line+"\n"
+            glpre=clean_data
+            gla=original_line
+            glb=re.sub(r"[\s]+", " ", glb_)+"\n"   # glossing b
+            glc=re.sub(r"[\s]+", " ", glc_)+"\n"    # glossing c
+            contents[z+1]=glb
+            contents[z+2]=glc
+        a_file = open(file_name+".txt", "w",)
+        a_file = open(file_name+".txt", "w", encoding="utf-8")
+        a_file.writelines(contents)
+        a_file.close()
+        
+        
